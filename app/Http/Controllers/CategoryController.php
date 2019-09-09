cdc1b6f1ae1c9d801f6cf417d54d97eb7a93a31c
Rifat <rifat.aiub@hotmail.com>
2019-09-09 17:57:20 +0600

    Rifat

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,6 +8,10 @@ use Yajra\DataTables\DataTables;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function index()
     {
